7bd9e12d09012f5dcbba22c368a8bf5d8794103d
unknown <tyler.b.porter@gmail.com>
2019-06-07 21:02:59 -0500

    Retrieves and creates note data from MongoDB cloud database

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,45 @@ const mongoUrl = 'mongodb+srv://test:test@cluster0-9igoz.mongodb.net/test?retryW
 
 let getNotes = (req: any, res: any) => {
 
-	MongoClient.connect( mongoUrl, { useNewUrlParser: true }, (err: any) => {
-		if (err) {
-			console.log(err.message);
-		} else {
-			console.log("Successfully connected!");
-		}
+	MongoClient.connect( mongoUrl, { useNewUrlParser: true }, (err: any, client: any) => {
+		
+		if (err) throw err;
+
+		var db = client.db('note');
+
+		var notes = db.collection('notes').find({});
+
+		notes.each( (err: any, note: any) => {
+			console.log(note);
+		});
+
+		client.close();
 	});
+
+}
+
+let createNote = (req: any, res: any) => {
+
+	MongoClient.connect( mongoUrl, { useNewUrlParser: true }, (err: any, client: any) => {
+
+		if (err) throw err;
+
+		var db = client.db('note');
+		
+		db.collection('notes').insertOne({
+			title: "Testing!",
+			content: "This is just a test, for real." 
+		});
+
+		client.close();
+	});	
 
 }
 
 // API endpoints
 
 app.get( '/notes', getNotes );
+app.get( '/notes/create', createNote );
 
 app.listen( port, () => {
 	console.log( `server started at http://localhost:${ port }` );
