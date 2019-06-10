acb32be38f7eed22dd78b045301b04f318e36a2e
unknown <tyler.b.porter@gmail.com>
2019-06-10 12:14:18 -0500

    Added functionality to delete notes from DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express = require('express');
 import bodyParser = require('body-parser');
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectID } from 'mongodb';
 
 // Express app config
 const app = express();
@@ -8,7 +8,7 @@ const mongoUrl = 'mongodb+srv://test:test@cluster0-9igoz.mongodb.net/test?retryW
 const port = process.env.PORT || 3000;
 
 // body-parser config
-// app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 let getNotes = (req: any, res: any) => {
@@ -49,7 +49,7 @@ let getNotes = (req: any, res: any) => {
 
 let createNote = (req: any, res: any) => {
 
-		let data = req.body;
+	let data = req.body;
 
 	MongoClient.connect( mongoUrl, { useNewUrlParser: true }, (err: any, client: any) => {
 
@@ -65,10 +65,31 @@ let createNote = (req: any, res: any) => {
 
 }
 
-// API endpoints
+let deleteNote = (req: any, res: any) => {
 
+	let id = req.params.id;
+
+	MongoClient.connect( mongoUrl, { useNewUrlParser: true }, (err: any, client: any) => {
+
+		if (err) throw err;
+		const db = client.db('note');
+		
+		db
+			.collection('notes')
+			.deleteOne({_id: new ObjectID(id)}, (err: any) => {
+				if (err) throw err;
+		});
+
+		res.send("complete")
+	
+	});
+	
+}
+
+// API endpoints for notes
 app.get( '/api/notes', getNotes );
 app.post( '/api/notes/create', createNote );
+app.delete( '/api/notes/delete/:id', deleteNote );
 
 app.listen( port, function () {
 	console.log(`Server started, listening on port ${port}.`)
