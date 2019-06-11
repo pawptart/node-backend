46056ca276baee4b22e78a919de27667558147f3
unknown <tyler.b.porter@gmail.com>
2019-06-10 22:56:16 -0500

    Update CORS for Heroku Deploy

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,15 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+// Allow Cross Domain Requests
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", '*');
+	// res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	next();
+});
+
 let getNotes = (req: any, res: any) => {
 
 	MongoClient.connect( mongoUrl, { useNewUrlParser: true }, (err: any, client: any) => {
